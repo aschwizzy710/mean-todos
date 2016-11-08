c322217d8e11002efa2b0636b871e0ddd07a58db
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-08 18:37:02 -0500

    added get functionality

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -7,7 +7,18 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
 
-router.get('/todos', function(req, res){});
+router.get('/todos', function(req, res){
+  Todo.find({}, function(err, foundTodos){
+    if(err){
+      res.status(500).json({
+        err: err
+      });
+    }
+    res.status(200).json({
+      todos: foundTodos
+    });
+  });
+});
 router.get('/todos/:id', function(req, res){});
 router.post('/todos', function(req, res){
   console.log(req.body);
