b7efddd3a013adb3f2415e64f3339890510f4968
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-07 20:44:54 -0500

    implemented the create operation through the post route

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var server = express();
+todoRouter = require('./server/routers/todo.router.js');
+var mongoose = require('mongoose');
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+mongoose.connect(mongoURI);
 
 var port = process.env.PORT || 8080;
 
@@ -8,6 +12,8 @@ server.use(express.static(__dirname + '/public'));
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root:__dirname});
 });
+
+server.use(todoRouter);
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
